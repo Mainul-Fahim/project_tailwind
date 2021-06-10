88118cfd7e8c016323753731d5b609f6337e33dd
Mainul-Fahim <36801303+Mainul-Fahim@users.noreply.github.com>
2021-06-11 02:12:44 +0600

    done

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import ImageOne from '../images/egg.jpg';
+import ImageOne from '../images/Juicest-Burger-Everfeature.jpg.jpg';
 import ImageTwo from '../images/egg-2.jpg';
 
 const Content = () => {
   return (
     <>
       <div className='flex flex-col justify-center items-center bg-white h-screen font-mono py-40'>
-        <img src={ImageOne} alt='egg' className='h-full object-contain rounded mb-20 shadow' />
+        <img src={ImageOne} alt='egg' className='h-full rounded mb-20 shadow' />
         <div className='flex flex-col justify-center items-center'>
-          <h2 className='text-2xl mb-2'>Egg Muffins</h2>
-          <p className='mb-2'>Cripsy, delicious, and nutritious</p>
+          <h2 className='text-2xl mb-2'>Juciest Burger</h2>
+          <p className='mb-2'>Crispy, delicious, and nutritious</p>
           <span>$16</span>
         </div>
       </div>
@@ -17,7 +17,7 @@ const Content = () => {
         <img src={ImageTwo} alt='egg' className='h-full rounded mb-20 shadow' />
         <div className='flex flex-col justify-center items-center'>
           <h2 className='text-2xl mb-2'>Egg Salad</h2>
-          <p className='mb-2'>Cripsy, delicious, and nutritious</p>
+          <p className='mb-2'>Crispy, delicious, and nutritious</p>
           <span>$18</span>
         </div>
       </div>
